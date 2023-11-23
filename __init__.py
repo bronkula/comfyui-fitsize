047853c9d2bc42f4324abd9ff8d59082c0987721
Hamilton <hamdiggy@gmail.com>
2023-11-23 03:58:45 -0800

    added fit resize

--- __init__.py
+++ __init__.py
@@ -1,15 +1,17 @@
-from .nodes import FitSize, FitSizeFromImage
+from .nodes import FitSize, FitSizeFromImage, FitResizeImage
 
 # A dictionary that contains all nodes you want to export with their names
 # NOTE: names should be globally unique
 NODE_CLASS_MAPPINGS = {
     "FitSizeByMax": FitSize,
     "FitSizeFromImage": FitSizeFromImage,
+    "FitResizeImage": FitResizeImage,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "FitSizeByMax": "Fit Size",
     "FitSizeFromImage": "Fit Size From Image",
+    "FitResizeImage": "Fit Resize Image",
 }
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
--- nodes.py
+++ nodes.py
@@ -1,14 +1,20 @@
+from PIL import Image
+import numpy as np
+import torch
 
-
-
+# Tensor to PIL
+def tensor2pil(image):
+    return Image.fromarray(np.clip(255. * image.cpu().numpy().squeeze(), 0, 255).astype(np.uint8))
+    
+# PIL to Tensor
+def pil2tensor(image):
+    return torch.from_numpy(np.array(image).astype(np.float32) / 255.0).unsqueeze(0)
 
 def get_max_size (width, height, max):
     aspect_ratio = width / height
 
     fit_width = max
     fit_height = max
-
-    print(aspect_ratio, fit_width, fit_height, max, width, height)
 
     if aspect_ratio > 1:
         fit_height = int(max / aspect_ratio)
@@ -69,3 +75,47 @@ class FitSizeFromImage:
         size = get_image_size(image)
         values = get_max_size(size[0], size[1], max_size)
         return values
+    
+class FitResizeImage:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "image": ("IMAGE",),
+                "max_size": ("INT", {"default": 768, "step": 8}),
+                "resampling": (["lanczos", "nearest", "bilinear", "bicubic"],),
+            }
+        }
+
+    RETURN_TYPES = ("IMAGE","INT","INT","FLOAT")
+    RETURN_NAMES = ("Image","Fit Width", "Fit Height", "Aspect Ratio")
+    FUNCTION = "fit_resize_image"
+
+    CATEGORY = "Fitsize"
+
+    def fit_resize_image (self, image, max_size=768, resampling="bicubic"):
+        size = get_image_size(image)
+        img = tensor2pil(image)
+
+        octalwidth = size[0] if size[0] % 8 == 0 else size[0] + (8 - size[0] % 8)
+        octalheight = size[1] if size[1] % 8 == 0 else size[1] + (8 - size[1] % 8)
+
+        new_width, new_height, aspect_ratio = get_max_size(octalwidth, octalheight, max_size)
+
+        print("new values",new_width, new_height, aspect_ratio)
+
+        resample_filters = {
+            'nearest': 0,
+            'bilinear': 2,
+            'bicubic': 3,
+            'lanczos': 1
+        }
+
+        print("Resampling:", resampling, resample_filters[resampling])
+
+        resized_image = img.resize((new_width, new_height), resample=Image.Resampling(resample_filters[resampling]))
+
+        return (pil2tensor(resized_image),new_width,new_height,aspect_ratio)
